9e200d05a0743b8e1f055ecba8769f204bfb4976
Nathan Kilcrease <natekilcrease@gmail.com>
2022-05-07 09:31:11 -0700

    fixed routes, re-wrote get and post, re-worked server.js

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,59 @@
+const noteRoute = require("express").Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+
+noteRoute.get("/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, noteData) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // console.log(noteData);
+
+      const parsedNotes = JSON.parse(noteData);
+      res.json(parsedNotes);
+    }
+  });
+});
+
+noteRoute.post("/notes", (req, res) => {
+  const { title, text } = req.body;
+  // get new notes --request
+  // use ID
+  // read existing db.json
+  // parse the data
+  // write to the file
+  // fs.writefile will replace anything previous
+  //
+  if (req.body) {
+    const noteNew = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    fs.readFile("./db/db.json", "utf8", (err, noteData) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(noteData);
+        parsedNotes.push(noteNew);
+
+        fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (err) =>
+          err
+            ? console.error(err)
+            : console.info(`\nData has been written to ${"./db/db.json"}`)
+        );
+      }
+    });
+    res.json("added note succesfully");
+  } else {
+    res.error("adding note Unsuccesful");
+  }
+});
+
+noteRoute.delete("/notes/:id", (req, res) => {
+    
+});
+
+module.exports = noteRoute;
--- Develop/server.js
+++ Develop/server.js
@@ -1,77 +1,25 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const db = require('./db/db.json');
-const PORT = 3001;
-const randomChar = require('./helpers/uuid');
-
+const api = require("./routes/apiRoutes");
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+
 
 app.use(express.static('public'));
-
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
-);
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-// unit 20 week 11 data persistance-- pay attention fs.readFile
-
-app.get('/api/notes', (req, res) => {
-  let noteValue = db;
-  res.json(noteValue);
-});
-
-app.post('/api/notes',(req, res) => {
-
-  req.body.id = db.length + 1;
-  let notes = db;
-  let noteIn = req.body;
-
-  notes.push(noteIn);
-  fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify(notes, null, 2)
-  );
-
-  res.json(noteIn);
-
-  // const { title, text } = req.body;
-  // const newNote = {
-  //   title,
-  //   text,
-  //   id: uuidv1(),
-  // };
-
-  // fs.readFile('./db/db.json', 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.error(err);
-  //   } else {
-  //     // Convert string into JSON object
-  //     const parsedNote = JSON.parse(data);
-
-  //     // Add a new review
-  //     parsedNote.push(newNote);
-
-  //     // Write updated reviews back to the file
-  //     fs.writeFile(
-  //       './db/db.json',
-  //       JSON.stringify(parsedNote, null, 4),
-  //       (writeErr) =>
-  //         writeErr
-  //           ? console.error(writeErr)
-  //           : console.info('Successfully updated notes!')
-  //     );
-  //   }
-  // });
-
-});
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT} 🚀`)
